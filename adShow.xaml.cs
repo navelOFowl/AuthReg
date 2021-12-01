9ac55c0b11c9dda1f8802c9f45211a738aabce2e
Курников Дмитрий <smilyck9@outlook.com>
2021-12-01 14:42:43 +0300

    15 практика. Критическая ошибка

--- adShow.xaml.cs
+++ adShow.xaml.cs
@@ -20,10 +20,12 @@ namespace AuthReg
     /// </summary>
     public partial class adShow : Page
     {
+        List<Занятия> LessStart = Base.DB.Занятия.ToList();
         public adShow()
         {
             InitializeComponent();
-            lvLess.ItemsSource = Base.DB.Занятия.ToList();
+            lvLess.ItemsSource = LessStart;
+            tbCount.Text = "Найдено записей: " + LessStart.Count + "";
         }
         
         private void ButtDeleteClick(object sender, RoutedEventArgs e)
@@ -43,6 +45,124 @@ namespace AuthReg
             int lessId = Convert.ToInt32(buttEdit.Uid);  
             Занятия lessUpd = Base.DB.Занятия.FirstOrDefault(x => x.IDLesson == lessId); 
             FrameCl.AdminFrame.Navigate(new createEdit(lessUpd));
+        }
+
+        List<Занятия> LessFilter;
+        private void Filter()
+        {
+            int index = cbFilter.SelectedIndex;
+            if (index != 0)
+            {
+                LessFilter = LessStart.Where(x => x.IDLesson == index).ToList();
+            }
+            else
+            {
+                LessFilter = LessStart;
+            }
+
+            if (tbDateFilter != null)
+            {
+                LessFilter = LessFilter.Where(x => x.Дата == tbDateFilter.SelectedDate).ToList();
+            }
+
+            if (cbC.IsChecked == true)
+            {
+                LessFilter = LessFilter.Where(x => x.Курс == 2).ToList();
+            }
+            else
+            {
+                MessageBox.Show("ТЕСТ");
+            }
+            if (cbSharp.IsChecked == true)
+            {
+                LessFilter = LessFilter.Where(x => x.Курс == 1).ToList();
+            }
+            if (cbApp.IsChecked == true)
+            {
+                LessFilter = LessFilter.Where(x => x.Курс == 3).ToList();
+            }
+            if (cbGraph.IsChecked == true)
+            {
+                LessFilter = LessFilter.Where(x => x.Курс == 4).ToList();
+            }
+            if (cbTest.IsChecked == true)
+            {
+                LessFilter = LessFilter.Where(x => x.Курс == 5).ToList();
+            }
+            if (cbMath.IsChecked == true)
+            {
+                LessFilter = LessFilter.Where(x => x.Курс == 6).ToList();
+            }
+        }
+
+        private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbSharp_Checked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbSharp_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbC_Checked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbC_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbApp_Checked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbApp_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbGraph_Checked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbGraph_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void tbDateFilter_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbTest_Checked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbTest_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbMath_Checked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbMath_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Filter();
         }
     }
 }
